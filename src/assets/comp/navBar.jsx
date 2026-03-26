69bb3b9e5ffd00ada195880bda430c336df73836
salmon <alexis.salmon.ss@gmail.com>
2026-03-26 08:50:54 +0100

    changement de style de la nav bar

--- src/assets/comp/navBar.jsx
+++ src/assets/comp/navBar.jsx
@@ -15,8 +15,8 @@ export default function NavBar() {
 
   return (
     <nav
-      className={`navBar z-5 sticky top-4 left-0 h-16 bg-gray-800 text-white flex flex-row items-center mx-auto transition-all duration-300
-        ${shrink ? "w-1/2 justify-around rounded-full" : "w-full justify-between rounded-none"}
+      className={`navBar z-5 sticky top-4 left-0 h-16 backdrop-blur-md bg-[#00000070] text-white flex flex-row items-center mx-auto transition-all duration400
+        ${shrink ? "w-1/2 justify-around rounded-full" : "w-full justify-between rounded-none "}
       `}
     >
       <div
@@ -29,13 +29,13 @@ export default function NavBar() {
         </a>
       </div>
       <div
-        className={`link flex mx-12 text-xs justify-center items-center transition-all duration-300
-        ${shrink ? "gap-4" : "gap-12"}
+        className={`link flex mx-12 text-sm justify-center items-center transition-all duration-300
+        ${shrink ? "gap-4" : "gap-8"}
       `}
       >
         <a href="#concept">Concept</a>
         <a href="#">Experience</a>
-        <a href="#">Tarification</a>
+        <a href="#tarif">Tarification</a>
         <a href="#contact">Contact</a>
       </div>
     </nav>
