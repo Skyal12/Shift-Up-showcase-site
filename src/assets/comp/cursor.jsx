96a15258553d974f4d4625bdfa72ca45cba66156
goatdryan <mwanzaryan01@gmail.com>
2026-04-01 16:34:21 +0200

    cursor

--- src/assets/comp/cursor.jsx
+++ src/assets/comp/cursor.jsx
@@ -14,23 +14,16 @@ export default function Cursor() {
     document.addEventListener("mousemove", handleMouseMove);
 
     const handleMouseEnter = () => {
-      if (cursor) {
-        cursor.classList.remove("w-4", "h-4");
-        cursor.classList.add("w-12", "h-12");
-      }
+      if (cursor) cursor.classList.add("w-12 h-12").remove("w-4 h-4");
     };
 
     const handleMouseLeave = () => {
-      if (cursor) {
-        cursor.classList.remove("w-12", "h-12");
-        cursor.classList.add("w-4", "h-4");
-      }
+      if (cursor) cursor.classList.remove("w-12 h-12").add("w-4 h-4");
     };
 
     const clickableElements = document.querySelectorAll(
-      "a, button, [role='button']"
+      "a, button, [role='button']",
     );
-    
     clickableElements.forEach((el) => {
       el.addEventListener("mouseenter", handleMouseEnter);
       el.addEventListener("mouseleave", handleMouseLeave);
@@ -44,10 +37,9 @@ export default function Cursor() {
       });
     };
   }, []);
-
   return (
     <>
-      <div className="cursor fixed z-[10000] top-0 left-0 w-4 h-4 bg-[#ff4800] opacity-75 rounded-full pointer-events-none transition-all duration-150 ease-out"></div>
+      <div className="cursor fixed z-[9999] top-0 left-0 w-4 h-4 bg-[#ff4800] opacity-75 rounded-full pointer-events-none"></div>
     </>
   );
 }
